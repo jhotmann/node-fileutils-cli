b1926f7363f2951c4499f449f70df3361842938d
Jordan Hotmann <jhot@protonmail.com>
2020-12-01 09:42:30 -0700

    Download progress, retries, redirects

--- src/yargs/download.js
+++ src/yargs/download.js
@@ -2,6 +2,7 @@ const chalk = require('chalk');
 const database = require('../util/database');
 const download = require('download');
 const path = require('path');
+const ProgressBar = require('progress');
 const {URL} = require('url');
 
 module.exports.command = 'download <url> [output-directory]';
@@ -16,6 +17,23 @@ exports.builder = (yargs) => {
         string: true,
         describe: 'override the file name of the downloaded file'
       },
+      t: {
+        alias: 'tries',
+        number: true,
+        default: 20,
+        describe: 'Number of retries. To disable retries, set to 0'
+      },
+      'max-redirect': {
+        alias: 'max-redirects',
+        number: true,
+        default: 20,
+        describe: 'The maximum number of redirections to follow for a resource'
+      },
+      q: {
+        alias: 'quiet',
+        boolean: true,
+        describe: 'Turn off logging and progress bar'
+      },
       v: {
         alias: 'verbose',
         boolean: true
@@ -29,12 +47,27 @@ exports.builder = (yargs) => {
 exports.handler = async function (argv) {
   const outDir = argv.outputDirectory ?? '.';
   let options = {
-    filename: path.basename(new URL(argv.url).pathname)
+    filename: path.basename(new URL(argv.url).pathname),
+    retry: argv.tries,
+    maxRedirects: argv.maxRedirects
   };
   if (argv.filename) options.filename = argv.filename;
+  let bar;
   await download(argv.url, outDir, options)
+    .on('response', res => {
+      if (argv.verbose) console.log(`${res.url} - ${res.statusCode}`);
+      const len = parseInt(res.headers['content-length'], 10) || 0;
+      if (!argv.quiet) {
+        bar = new ProgressBar('  downloading [:bar] :rate/Kbps :percent :etas', {
+          complete: '=',
+          incomplete: ' ',
+          width: 30,
+          total: len/1024
+        });
+      }
+    })
     .on('downloadProgress', progress => {
-      console.log(progress);
+      if (!argv.quiet) bar.update(progress?.percent ?? 1);
     })
     .catch(err => {
       console.log(chalk`{red ${err}}`);
